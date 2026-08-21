3094ab9e96a287af58e6ad57b54fb275476aa34d
IgorStrzezysz1 <93446017+IgorStrzezysz1@users.noreply.github.com>
2026-08-21 21:33:18 +0200

    New update name changed in Exercises

--- src/Exercise_5_cash_machine.java
+++ src/Exercise_5_cash_machine.java
@@ -3,97 +3,102 @@ import java.util.Scanner;
 public class Exercise_5_cash_machine {
 
     public static void main(String[] args) {
-        while (true) {
-            int correctPin = 1234;
-            System.out.println("Podaj PIN: ");
-            Scanner PIN_Number = new Scanner(System.in);
-            int enteredPin = PIN_Number.nextInt();
-            System.out.println("=== BANKOMAT ===");
 
-            if (enteredPin == correctPin) {
+        Scanner scanner = new Scanner(System.in);
+
+        int correctPin = 1234;
+        double balance = 22660.89;
+
+        // LOGOWANIE PIN
+        while (true) {
+
+            System.out.println("Podaj PIN:");
+            int enteredPin = scanner.nextInt();
+
+            if (enteredPin != correctPin) {
+                System.out.println("Niepoprawny PIN! Spróbuj ponownie.");
+                continue;
+            }
+
+            System.out.println("PIN poprawny!");
+
+            // MENU BANKOMATU
+            while (true) {
+
+                System.out.println();
                 System.out.println("=== BANKOMAT ===");
                 System.out.println("1. Sprawdź saldo");
                 System.out.println("2. Wpłać pieniądze");
                 System.out.println("3. Wypłać pieniądze");
                 System.out.println("4. Wyjście");
-                System.out.println(" Wybierz opcję:");
-            }
-                else {
+                System.out.println("================");
+                System.out.println("Wybierz opcję:");
 
-                    System.out.println("Nie poprawny PIN, popraw prosze ");
-                    System.out.println("Podaj PIN: ");
-                    Scanner PIN_Number_another = new Scanner(System.in);
-                    int another_PIN_Numer = PIN_Number_another.nextInt();
-                    if (another_PIN_Numer == correctPin) {
-                    //    System.out.println("=== BANKOMAT ===");
-                    }
-                    else {
-                    while (another_PIN_Numer == correctPin) {}
-                    }
-            }
+                int option = scanner.nextInt();
 
-                switch (enteredPin){
+                switch (option) {
+
+                    // SPRAWDZENIE SALDA
                     case 1:
-                        System.out.println("Masz na koncie 22 660,89zł");
+                        System.out.println("Twoje saldo wynosi: " + balance + " zł");
                         break;
 
-                        case 2:
-                            System.out.println("Ile chcesz wypłącić?");
-                            Scanner money_deploy_number = new Scanner(System.in);
-                            System.out.println("Chcesz wypłącić " + money_deploy_number + " ? ");
-                            System.out.println("1. tak");
-                            System.out.println("2. Nie");
-                            switch (money_deploy_number.nextInt()) {
-                                case 1:
-                                    System.out.println("Prosze oto twoja kwota " + money_deploy_number + "do zobaczania " );
 
-                                case 2:
-                                    System.out.println("Czy chcesz wykonać inną operację?");
-                                    System.out.println("1. Tak");
-                                    System.out.println("2. Nie");
+                    // WPŁATA PIENIĘDZY
+                    case 2:
+                        System.out.println("Ile chcesz wpłacić?");
 
-                                    Scanner scanner = new Scanner(System.in);
-                                    int decision = scanner.nextInt();
+                        double deposit = scanner.nextDouble();
 
-                                    switch (decision) {
-                                        case 1:
-                                            System.out.println("Tak");
-                                            break;
+                        if (deposit <= 0) {
+                            System.out.println("Kwota musi być większa od 0!");
+                        } else {
+                            balance = balance + deposit;
 
-                                        case 2:
-                                            System.out.println("Nie");
-                                            break;
+                            System.out.println("Wpłaciłeś: " + deposit + " zł");
+                            System.out.println("Nowe saldo: " + balance + " zł");
+                        }
 
-                                        default:
-                                            System.out.println("Nieprawidłowy wybór!");
-                                            break;
-                                    }
+                        break;
 
-                            }
+
+                    // WYPŁATA PIENIĘDZY
+                    case 3:
+                        System.out.println("Ile chcesz wypłacić?");
+
+                        double withdraw = scanner.nextDouble();
+
+                        if (withdraw <= 0) {
+                            System.out.println("Kwota musi być większa od 0!");
+                        } else if (withdraw > balance) {
+                            System.out.println("Nie masz wystarczających środków!");
+                            System.out.println("Twoje saldo: " + balance + " zł");
+                        } else {
+                            balance = balance - withdraw;
+
+                            System.out.println("Wypłaciłeś: " + withdraw + " zł");
+                            System.out.println("Pozostało na koncie: " + balance + " zł");
+                        }
+
+                        break;
+
+
+                    // WYJŚCIE
+                    case 4:
+                        System.out.println("Wyjmij kartę.");
+                        System.out.println("Do widzenia!");
+
+                        scanner.close();
+                        return;
+
+
+                    // ZŁA OPCJA
+                    default:
+                        System.out.println("Nieprawidłowa opcja!");
+                        System.out.println("Wybierz 1, 2, 3 lub 4.");
+                        break;
                 }
-
-            } else {
-                System.out.println("Nieprawidłowy PIN!");
-                System.out.println("Chcesz zmienić PIN?");
-                System.out.println("Tak ?");
-                System.out.println("=============");
-                System.out.println("Nie i anuluj operacje, wyjmij karte z bankomatu");
-                Scanner new_PIN_number = new Scanner(System.in);
-                String answer = PIN_Number.next();
-                if (answer.equals("Tak")) {
-                    System.out.println("Podaj nowy PIN: ");
-                    Scanner PIN_Number2 = new Scanner(System.in);
-                    int enteredPin2 = PIN_Number2.nextInt();
-                    System.out.println("=== BANKOMAT ===");
-                    if (enteredPin2 == correctPin) {
-                        System.out.println("=== BANKOMAT ===");
-
-                    }
-                }
-
-
             }
-
         }
     }
 }
